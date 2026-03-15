0d1929b1c6be9352a757b59c45d72157d97cb9b5
shauntoal95 <shaunmichaeltoal@gmail.com>
2026-03-15 00:05:03 +0000

    Fix Supabase export

--- backend/supabase.js
+++ backend/supabase.js
@@ -1,9 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-import dotenv from 'dotenv';
+import { createClient } from "@supabase/supabase-js";
 
-dotenv.config();
-
-export const supabase = createClient(
+const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+
+export default supabase;
